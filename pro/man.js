d30c2211d37ae117fa605fb94c277739637871a9
ahmedosama2022 <109433889+ahmedosama2022@users.noreply.github.com>
2022-08-25 10:22:22 +0200

    Add files via upload

---
+++ pro/man.js
@@ -0,0 +1,19 @@
+
+
+
+let img = document.querySelector('.img');
+let contener = document.querySelector('.contener');
+
+function phons(phone) {
+    img.src = phone;
+
+}
+
+function colors(color) {
+    contener.style.background = color;
+    
+}
+
+
+
+
